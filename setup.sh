cdece356a9bc82d95465c8ead6d7368633c61a93
Mark Dunne <markd@deskgen.com>
2018-03-06 16:08:43 +0000

    sudo chsh

--- setup.sh
+++ setup.sh
@@ -5,5 +5,5 @@
 sudo apt-get update
 sudo apt-get install zsh -y
 
-chsh -s $(which zsh)
+sudo chsh markd -s /usr/bin/zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
